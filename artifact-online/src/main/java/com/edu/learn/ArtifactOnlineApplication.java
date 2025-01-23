d4110dff409e88c423db9bdd6f554f3a7bb2480c
vbasa <venkat2toc@gmail.com>
2025-01-23 23:19:48 +0530

    1st commit

---
+++ artifact-online/src/main/java/com/edu/learn/ArtifactOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.edu.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifactOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifactOnlineApplication.class, args);
+	}
+
+}
---
+++ artifact-online/src/test/java/com/edu/learn/ArtifactOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
